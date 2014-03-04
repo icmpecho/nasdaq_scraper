467b90b4452222e2be60b5f0ff4c05c0d0946751
IcmpEcho <pinnapong@gmail.com>
2014-03-04 23:34:25 +0700

    Check for valid result in cli wrapper

--- lib/nasdaq_scraper/cli/application.rb
+++ lib/nasdaq_scraper/cli/application.rb
@@ -8,6 +8,10 @@ module NasdaqScraper
 			desc 'scrape [URL]', 'Scrape the [URL] for NASDAQ index'
 			def scrape(url)
 				data = NasdaqScraper::scrape_url(url)
+				if(!data)
+					puts "An error occured."
+					return
+				end
 				puts data.to_str
 			end
 
