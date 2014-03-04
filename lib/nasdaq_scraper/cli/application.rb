b41e1cc828f58b1577e284d9a5cd5302d38bdb9a
IcmpEcho <pinnapong@gmail.com>
2014-03-04 23:02:07 +0700

    Create cli application to wrap the lib

---
+++ lib/nasdaq_scraper/cli/application.rb
@@ -0,0 +1,18 @@
+require 'thor'
+require 'nasdaq_scraper'
+
+module NasdaqScraper
+	module Cli
+		class Application < Thor
+			
+			desc 'scrape URL', 'Scrape the URL for NASDAQ index'
+			def scrape(url)
+				data = NasdaqScraper::scrape_url(url)
+				puts "NASDAQ:"
+				puts "Index: #{data[:index]}"
+				puts "Change: #{data[:change]}"
+			end
+
+		end
+	end
+end
